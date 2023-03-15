251c0b04f995862f049764a23d1cbe63787c9116
IceyPiter <manopar24@gmail.com>
2023-03-14 21:53:27 -0300

    Atividade atvTeste

---
+++ Atividades/AtvTeste/notebook/AtvTeste.java
@@ -0,0 +1,10 @@
+package Atividades.AtvTeste.notebook;
+import java.util.Scanner; //Importe da biblioteca Scanner
+
+public class AtvTeste{ //Criação da classe atvTeste
+    public static void main(String[] args) { //Cria a funcao main
+        System.out.println("Realizado com Sucesso"); //Mostra a mensagem na tela
+        Scanner line = new Scanner(System.in); //Cria o input
+        String out = line.nextLine(); //Pede a saida
+    }
+} 
